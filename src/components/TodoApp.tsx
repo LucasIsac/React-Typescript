97d726783cea2c4a28f63960ddfa0c7829c6181a
LucasIsac <lisaacdlf21@gamil.com>
2025-03-01 20:07:04 -0300

    Proyecto Final de Lista de Tareas con React y Typescript

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { ListaTareas } from './ListaTareas';
+
+export const TodoApp = () => {
+  const [nuevaTarea, setNuevaTarea] = useState<string>('');
+  const [listaTareas, setListaTareas] = useState<string[]>([]);
+
+  const handleAdTask = () => {
+        if (nuevaTarea.trim() === '') return
+        setListaTareas(tareasAnteriores => [...tareasAnteriores, nuevaTarea])
+        setNuevaTarea('')
+  }
+
+  const handleBorrarTarea = (index: number) => {
+    setListaTareas(tareas => tareas.filter((_, i) => i !== index))
+  }
+
+  return (
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input
+        type="text"
+        value={nuevaTarea}
+        onChange={(e) => setNuevaTarea(e.target.value)}
+        placeholder="Nueva tarea"
+      />
+      <button onClick={handleAdTask}>Agregar</button>
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}></ListaTareas>
+    </div>
+  );
+};
